4e1402f6a46c68ac1c5c095ecda3898ce2c7a016
Elkin A Saavedra C <elkin_saacar@hotmail.com>
2022-09-23 18:03:32 -0500

    Elkin Saavedra challegne

--- src/components/kanban-board/index.js
+++ src/components/kanban-board/index.js
@@ -1,60 +1,67 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./index.css";
 
-export default class KanbanBoard extends Component {
-  constructor(props) {
-    super(props);
-    // Each task is uniquely identified by its name. 
-    // Therefore, when you perform any operation on tasks, make sure you pick tasks by names (primary key) instead of any kind of index or any other attribute.
-    this.state = {
-        tasks: this.props.tasks
-    };
-    this.stagesNames = ['Backlog', 'To Do', 'Ongoing', 'Done'];
+const KanbanBoard = (props) => {
+
+  const { tasks } = props;
+  // console.log(props);
+  const stagesNames = ['Backlog', 'To Do', 'Ongoing', 'Done'];
+  const IDsStages = [0, 1, 2, 3];
+
+  let stagesTasks = [];
+  console.log(tasks);
+  for (let i = 0; i < stagesNames.length; ++i) {
+    stagesTasks.push([]);
   }
- 
-  render() {
-    const { tasks } = this.state;
+  for (let task of tasks) {
+    const stageId = task.stage;
+    stagesTasks[stageId].push(task);
+  }
 
-    let stagesTasks = [];
-    for (let i = 0; i < this.stagesNames.length; ++i) {
-      stagesTasks.push([]);
-    }
-    for (let task of tasks) {
-      const stageId = task.stage;
-      stagesTasks[stageId].push(task);
-    }
+  const taskBack = (name) => {
+    console.log(`taskBack ${name}`);
+  }
 
-    return (
-      <div className="mt-20 layout-column justify-content-center align-items-center">
-        <div className="mt-50 layout-row">
-            {stagesTasks.map((tasks, i) => {
-                return (
-                    <div className="card outlined ml-20 mt-0" key={`${i}`}>
-                        <div className="card-text">
-                            <h4>{this.stagesNames[i]}</h4>
-                            <ul className="styled mt-50" data-testid={`stage-${i}`}>
-                                {tasks.map((task, index) => {
-                                    return <li className="slide-up-fade-in" key={`${i}${index}`}>
-                                      <div className="li-content layout-row justify-content-between align-items-center">
-                                        <span data-testid={`${task.name.split(' ').join('-')}-name`}>{task.name}</span>
-                                        <div className="icons">
-                                          <button className="icon-only x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-back`}>
-                                            <i className="material-icons">arrow_back</i>
-                                          </button>
-                                          <button className="icon-only x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-forward`}>
-                                            <i className="material-icons">arrow_forward</i>
-                                          </button>
-                                        </div>
+  const taskNext = (name) => {
+    console.log(`taskNext ${name}`);
+  }
+
+  return (
+    <div className="mt-20 layout-column justify-content-center align-items-center">
+      <div className="mt-50 layout-row">
+          {stagesTasks.map((tasks, i) => {
+              let showButtonBack = (i == IDsStages[0]) ? false : true;
+              let showButtonNext = (i == IDsStages[ (IDsStages.length)-1 ]) ? false : true;
+
+              return (
+                  <div className="card outlined ml-20 mt-0" key={`${i}`}>
+                      <div className="card-text">
+                          <h4>{stagesNames[i]}</h4>
+                          <ul className="styled mt-50" data-testid={`stage-${i}`}>
+                              {tasks.map((task, index) => {
+                                  return <li className="slide-up-fade-in" key={`${i}${index}`}>
+                                    <div className="li-content layout-row justify-content-between align-items-center">
+                                      <span data-testid={`${task.name.split(' ').join('-')}-name`}>{task.name}</span>
+                                      <div className="icons">
+                                        <button className={`icon-only x-small mx-2 ${(showButtonBack) ? `` : `disabled`} `} data-testid={`${task.name.split(' ').join('-')}-back`} onClick={()=>taskBack(task.name)}>
+                                          <i className="material-icons">arrow_back</i>
+                                        </button>
+                                        <button className={`icon-only x-small mx-2 ${(showButtonNext) ? `` : `disabled`} `} data-testid={`${task.name.split(' ').join('-')}-forward`} onClick={()=>taskNext(task.name)}>
+                                          <i className="material-icons">arrow_forward</i>
+                                        </button>
                                       </div>
-                                    </li>
-                                })}
-                            </ul>
-                        </div>
-                    </div>
-                )
-            })}
-        </div>
+                                    </div>
+                                  </li>
+                              })}
+                          </ul>
+                      </div>
+                  </div>
+              )
+          })}
       </div>
-    );
-  }
-}
+    </div>
+  );
+  
+};
+
+export default KanbanBoard;
